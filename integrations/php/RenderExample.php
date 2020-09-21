371ad94ca199da3042c0421cc4c562a03c826cae
Arash Paknezad <arashpaknezad@MacBook-Pro-van-Arash.local>
2020-09-21 13:49:16 +0200

    chore: gitignore

--- integrations/php/RenderExample.php
+++ integrations/php/RenderExample.php
@@ -1,8 +1,11 @@
 <?php
 error_reporting(E_ALL ^ E_WARNING);
-
 require __DIR__ . '/vendor/autoload.php';
 
-$instance = new Wasm\Instance(__DIR__ . '/wasmer_template_renderer.wasm');
+$wasmFilePath = realpath('../../target/wasm32-unknown-unknown/debug/wasmer_template_renderer.wasm');
+
+var_dump($wasmFilePath);
+
+$instance = new Wasm\Instance($wasmFilePath);
 
 var_dump($instance);
