4f960ba7a5a3a6e644dcccf92d0e91abf4571472
Arash Paknezad <arashpaknezad@MacBook-Pro-van-Arash.local>
2020-11-19 20:28:04 +0100

    ci(netlify): testing netlify function

--- netlify.sh
+++ netlify.sh
@@ -6,4 +6,4 @@ rustup target add wasm32-unknown-unknown
 
 cargo build --target wasm32-unknown-unknown 
 
-cp target/wasm32-unknown-unknown/debug/wasmer_template_renderer.wasm functions/
+cp target/wasm32-unknown-unknown/debug/wasmer_template_renderer.wasm functions/test
