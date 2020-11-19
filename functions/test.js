4355a902a2cff2ca933445bf1c8a92b9a3790da8
Arash Paknezad <arashpaknezad@MacBook-Pro-van-Arash.local>
2020-11-19 17:52:51 +0100

    ci(netlify): testing netlify function

--- functions/test.js
+++ functions/test.js
@@ -3,9 +3,7 @@ const path = require('path');
 
 const WasmHandlebars = require('../integrations/js/shared/wasm-handlebars');
 
-const wasmFilePath = path.resolve(__dirname, 
-  '../target/wasm32-unknown-unknown/debug/wasmer_template_renderer.wasm'
-);
+const wasmFilePath = path.resolve('wasmer_template_renderer.wasm');
 
 const wasmBytes = fs.readFileSync(wasmFilePath);
 
