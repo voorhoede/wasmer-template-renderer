67ec1b95b5c592f2022d2d64d589602865c3f9ac
Arash Paknezad <arashpaknezad@MacBook-Pro-van-Arash.local>
2020-11-19 20:05:02 +0100

    ci(netlify): testing netlify function

--- netlify.sh
+++ netlify.sh
@@ -1,9 +1,9 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-source ~/.cargo/env
+# source ~/.cargo/env
 
-rustup target add wasm32-unknown-unknown
+# rustup target add wasm32-unknown-unknown
 
-cargo build --target wasm32-unknown-unknown 
+# cargo build --target wasm32-unknown-unknown 
 
-cp target/wasm32-unknown-unknown/debug/wasmer_template_renderer.wasm functions/
+# cp target/wasm32-unknown-unknown/debug/wasmer_template_renderer.wasm functions/
